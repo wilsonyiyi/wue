395817baefbacb9ab151682bdd9c4e72c57072c5
wangweiwei <will_workhard@163.com>
2018-07-08 01:01:18 +0800

    icon左右设置

--- src/button.vue
+++ src/button.vue
@@ -1,15 +1,35 @@
 <template>
   <div>
-    <button class="g-button">按钮</button>
+    <button class="g-button" :class="iconPos">
+      <svg class="icon">
+        <use :xlink:href="`#i-${icon}`"></use>
+      </svg>
+      <div class="content">
+        <slot></slot>
+      </div>
+    </button>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-export default {}
+export default {
+  props: {
+    icon: String,
+    iconPosition: String
+  },
+  computed: {
+    iconPos: function () {
+      return `icon-${this.iconPosition}`
+    }
+  }
+}
 </script>
 
 <style scoped lang="scss">
 .g-button {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
   margin: 20px;
   padding: 0 1em;
   height: var(--button-height);
@@ -26,6 +46,14 @@ export default {}
   }
   &:focus {
     outline: none;
+  }
+  > {
+    .icon{order: 1};
+    .content{order: 2};
+  }
+  &.icon-right{
+    .icon{order: 2};
+    .content{order: 1};
   }
 }
 </style>
