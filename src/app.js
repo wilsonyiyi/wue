694dc36bf2c761e6760734ffbd681b625514e730
Willworkgogogo <will_workhard@163.com>
2018-08-08 00:01:03 +0800

    使用chai-spies监听回调函数

--- src/app.js
+++ src/app.js
@@ -15,18 +15,57 @@ new Vue({
 })
 
 import chai from 'chai'
+import spies from 'chai-spies' // 用于函数监听调用
+chai.use(spies)
 const expect = chai.expect
 
+// test icon
 {
 	const Constructor = Vue.extend(Button)
-	const button = new Constructor({
+	const vm = new Constructor({
 		propsData: {
 			icon: 'setting'
 		}
 	})
 
-	button.$mount()
-	let useElement = button.$el.querySelector('use')
+	vm.$mount()
+	let useElement = vm.$el.querySelector('use')
 	let href = useElement.getAttribute('xlink:href')
 	expect(href).to.eq('#i-setting')
+	vm.$destroy()
+}
+
+// test loading
+{
+	const div = document.createElement('div')
+	document.body.appendChild(div)
+	const Constructor = Vue.extend(Button)
+	const vm = new Constructor({
+		propsData: {
+			icon: 'setting',
+			loading: true
+		}
+	})
+	vm.$mount(div)
+	let useElement = vm.$el.querySelector('use')
+	let href = useElement.getAttribute('xlink:href')
+	expect(href).to.eq('#i-loading')
+	// 释放内存
+	vm.$el.remove()
+	vm.$destroy()
+}
+
+// test click
+{
+	const Constructor = Vue.extend(Button)
+	const vm = new Constructor({
+		propsData: 'setting'
+	})
+	const spy = chai.spy(function () {})
+	vm.$mount()
+	vm.$on('click', spy)
+	vm.$el.click()
+	expect(spy).to.have.been.called()
+
+	vm.$destroy()
 }
