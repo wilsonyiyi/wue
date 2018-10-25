7ef7346ca98d40ce51ec76644a6985fb316f37f5
Willworkgogogo <will_workhard@163.com>
2018-10-25 15:46:21 +0800

    add row col

---
+++ src/components/col.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="col" :class="">
+    <slot></slot>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+	export default {
+		name: 'VVCol',
+		props: {
+			span: [Number, String]
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+  $background-color: rgba(0, 160, 233, 0.7);
+  $class_prefix: 'col_';
+  .col {
+    height: 100%;
+    background: $background-color;
+    width: 100%;
+    border: 1px solid #ccc;
+  }
+</style>
---
+++ src/components/row.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="row">
+    <slot></slot>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {}
+</script>
+
+<style lang="scss" scoped>
+  .row{
+    display: flex;
+  }
+</style>
