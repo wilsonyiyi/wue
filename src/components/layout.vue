8eb28bae1798121cd19eb5e440a4f4bc30487789
Willworkgogogo <will_workhard@163.com>
2019-02-12 09:33:34 +0800

    feature: layout components

---
+++ src/components/layout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>  
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: 'WLayout'
+  }
+</script>
+
+<style scoped lang="scss">
+  
+</style>
