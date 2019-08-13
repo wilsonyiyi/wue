fe8a8e54f90f33e8167452c9eef9ebd4dce8e9e4
Willworkgogogo <will_workhard@163.com>
2019-08-13 08:42:33 +0800

    update:  index export

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-import Button from "./src/components/button"
-import ButtonGroup from './src/components/button-group'
-import Icon from './src/components/icon'
+import Button from './src/components/button';
+import ButtonGroup from './src/components/button-group';
+import Icon from './src/components/icon';
+import Col from './src/components/col';
+import Row from './src/components/row';
+import Layout from './src/components/layout';
+import Content from './src/components/content';
+import Header from './src/components/header';
+import Sider from './src/components/sider';
+import Footer from './src/components/footer';
+import Toast from './src/components/toast';
 
-export {
-	Button,
-	ButtonGroup,
-	Icon
-}
+Layout.Header = Header;
+Layout.Content = Content;
+Layout.Sider = Sider;
+Layout.Footer = Footer;
+
+export { Button, ButtonGroup, Icon, Layout, Toast, Row, Col };
